dcabd52d5df1cbc7432c80bb003db3805831a904
sh1p1lov <sergey.shipilov1999@mail.ru>
2023-04-10 13:55:11 +0300

    Create WeatherFragment.kt and fragment_weather.xml

---
+++ app/src/main/java/com/sh1p1lov/weatherandroidapp/fragment/WeatherFragment.kt
@@ -0,0 +1,6 @@
+package com.sh1p1lov.weatherandroidapp.fragment
+
+import androidx.fragment.app.Fragment
+import com.sh1p1lov.weatherandroidapp.R
+
+class WeatherFragment : Fragment(R.layout.fragment_weather)
