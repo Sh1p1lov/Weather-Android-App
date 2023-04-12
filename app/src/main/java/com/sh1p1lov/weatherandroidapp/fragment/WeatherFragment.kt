fe9a9655d6c63bb5aebb2c4415ec7add72e41c88
sh1p1lov <sergey.shipilov1999@mail.ru>
2023-04-12 09:03:57 +0300

    Implementation of finding the current location

--- app/src/main/java/com/sh1p1lov/weatherandroidapp/fragment/WeatherFragment.kt
+++ app/src/main/java/com/sh1p1lov/weatherandroidapp/fragment/WeatherFragment.kt
@@ -1,12 +1,25 @@
 package com.sh1p1lov.weatherandroidapp.fragment
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.util.TypedValue
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.view.*
 import androidx.fragment.app.Fragment
 import com.sh1p1lov.weatherandroidapp.R
 import com.sh1p1lov.weatherandroidapp.databinding.FragmentWeatherBinding
+
+typealias OnCurrentLocationListener = (location: Location) -> Unit
 
 class WeatherFragment : Fragment(R.layout.fragment_weather) {
 
@@ -17,9 +30,23 @@ class WeatherFragment : Fragment(R.layout.fragment_weather) {
         private const val PROGRESS_VIEW_OFFSET_DP = 120f
     }
 
+    private val locationPermissionRequest = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            getCurrentLocation(l = onCurrentLocationListener)
+        }
+    }
+
+    private val onCurrentLocationListener: OnCurrentLocationListener = { location ->
+        Log.d("MainActivityTag", location.toString())
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentWeatherBinding.bind(view)
+
+        binding.btnUpdate.setOnClickListener {
+            getCurrentLocation(l = onCurrentLocationListener)
+        }
 
         val progressViewOffsetPx =
             TypedValue.applyDimension(
@@ -32,6 +59,48 @@ class WeatherFragment : Fragment(R.layout.fragment_weather) {
 
     fun updateToolbarPaddingTop(topPadding: Int) {
         binding.toolbar.updatePadding(top = topPadding)
+    }
+
+    private fun getCurrentLocation(l: OnCurrentLocationListener) {
+        when (
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+
+            PackageManager.PERMISSION_GRANTED -> {
+
+                val locManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                val criteria = Criteria()
+                val provider = locManager.getBestProvider(criteria, true)
+
+                provider?.let { providerName ->
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                        locManager.getCurrentLocation(
+                            providerName,
+                            null,
+                            requireContext().mainExecutor
+                        ) {
+                            l.invoke(it)
+                        }
+                    } else {
+                        @Suppress("DEPRECATION")
+                        locManager.requestSingleUpdate(
+                            providerName,
+                            {
+                                l.invoke(it)
+                            },
+                            Looper.getMainLooper()
+                        )
+                    }
+                }
+            }
+
+            else -> {
+                locationPermissionRequest.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+            }
+        }
     }
 
     override fun onDestroyView() {
