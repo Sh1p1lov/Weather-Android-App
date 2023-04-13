e228e72040adca58b41f114832349a588bf2d21d
sh1p1lov <sergey.shipilov1999@mail.ru>
2023-04-14 00:22:59 +0300

    Updated location acquisition logic and added language switching logic

--- app/src/main/java/com/sh1p1lov/weatherandroidapp/fragment/WeatherFragment.kt
+++ app/src/main/java/com/sh1p1lov/weatherandroidapp/fragment/WeatherFragment.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Criteria
 import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
@@ -18,6 +19,11 @@ import androidx.core.view.*
 import androidx.fragment.app.Fragment
 import com.sh1p1lov.weatherandroidapp.R
 import com.sh1p1lov.weatherandroidapp.databinding.FragmentWeatherBinding
+import com.sh1p1lov.weatherandroidapp.sharedprefs.AppPrefs
+import com.sh1p1lov.weatherandroidapp.viewmodel.WeatherFragmentViewModel
+import org.koin.android.ext.android.get
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.Locale
 
 typealias OnCurrentLocationListener = (location: Location) -> Unit
 
@@ -25,6 +31,8 @@ class WeatherFragment : Fragment(R.layout.fragment_weather) {
 
     private var _binding: FragmentWeatherBinding? = null
     private val binding: FragmentWeatherBinding get() = _binding!!
+    private val vm by viewModel<WeatherFragmentViewModel>()
+    private val appPref: AppPrefs = get()
 
     companion object {
         private const val PROGRESS_VIEW_OFFSET_DP = 120f
@@ -33,19 +41,100 @@ class WeatherFragment : Fragment(R.layout.fragment_weather) {
     private val locationPermissionRequest = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
         if (isGranted) {
             getCurrentLocation(l = onCurrentLocationListener)
+        } else {
+            appPref.getLocationLatitude()?.let { lat ->
+                vm.outdatedLocationError()
+                val lon = appPref.getLocationLongitude()!!
+                vm.updateData(latitude = lat, longitude = lon)
+
+            } ?: vm.locationError()
         }
     }
 
     private val onCurrentLocationListener: OnCurrentLocationListener = { location ->
-        Log.d("MainActivityTag", location.toString())
+        Log.d("WeatherFragmentTag", location.toString())
+
+        if (vm.error.value == WeatherFragmentViewModel.ERROR_CODE_LOCATION
+            || vm.error.value == WeatherFragmentViewModel.ERROR_CODE_OUTDATED_LOCATION
+        ) {
+            vm.noError()
+        }
+
+        appPref.saveLocationLatitudeAndLongitude(
+            latitude = location.latitude,
+            longitude = location.longitude
+        )
+
+        vm.updateData(
+            latitude = location.latitude,
+            longitude = location.longitude
+        )
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentWeatherBinding.bind(view)
+        updateLocale()
 
         binding.btnUpdate.setOnClickListener {
             getCurrentLocation(l = onCurrentLocationListener)
+        }
+
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.lang_ru -> {
+                    appPref.saveCurrentLanguage(AppPrefs.LANGUAGE_RU)
+                    updateLocale()
+                }
+                R.id.lang_en -> {
+                    appPref.saveCurrentLanguage(AppPrefs.LANGUAGE_EN)
+                    updateLocale()
+                }
+            }
+            true
+        }
+
+        vm.error.observe(viewLifecycleOwner) { errorCode ->
+            when(errorCode) {
+                WeatherFragmentViewModel.NO_ERROR -> {
+                    with(binding.tvError) {
+                        text = ""
+                        visibility = View.GONE
+                    }
+                }
+                WeatherFragmentViewModel.ERROR_CODE_LOCATION -> {
+                    with(binding.tvError) {
+                        text = requireContext().localeConfigContext().getText(R.string.location_error)
+                        visibility = View.VISIBLE
+                    }
+                }
+                WeatherFragmentViewModel.ERROR_CODE_OUTDATED_LOCATION -> {
+                    with(binding.tvError) {
+                        text = requireContext().localeConfigContext().getText(R.string.outdated_location_error)
+                        visibility = View.VISIBLE
+                    }
+                }
+                WeatherFragmentViewModel.ERROR_CODE_DATA_UPDATE -> {
+                    with(binding.tvError) {
+                        text = requireContext().localeConfigContext().getText(R.string.data_update_error)
+                        visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
+
+        vm.locationNameRu.observe(viewLifecycleOwner) { locationNameRu ->
+            appPref.saveLocationName(locationNameRu, AppPrefs.LANGUAGE_RU)
+            if (appPref.getCurrentLanguage() == AppPrefs.LANGUAGE_RU) {
+                binding.toolbar.title = locationNameRu
+            }
+        }
+
+        vm.locationNameEn.observe(viewLifecycleOwner) { locationNameEn ->
+            appPref.saveLocationName(locationNameEn, AppPrefs.LANGUAGE_EN)
+            if (appPref.getCurrentLanguage() == AppPrefs.LANGUAGE_EN) {
+                binding.toolbar.title = locationNameEn
+            }
         }
 
         val progressViewOffsetPx =
@@ -59,6 +148,42 @@ class WeatherFragment : Fragment(R.layout.fragment_weather) {
 
     fun updateToolbarPaddingTop(topPadding: Int) {
         binding.toolbar.updatePadding(top = topPadding)
+    }
+
+    private fun Context.localeConfigContext(): Context {
+        val language = appPref.getCurrentLanguage()
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        val config = this.resources.configuration
+        config.setLocale(locale)
+        return this.createConfigurationContext(config)
+    }
+
+    private fun updateLocale() {
+        val configContext = requireContext().localeConfigContext()
+
+        with(binding) {
+            tvWeatherTitle.text = when(appPref.getWeatherState()) {
+                AppPrefs.WEATHER_CLEAR -> configContext.getText(R.string.weather_clear)
+                AppPrefs.WEATHER_CLOUDY -> configContext.getText(R.string.weather_cloudy)
+                AppPrefs.WEATHER_PARTY_CLOUDY -> configContext.getText(R.string.weather_partly_cloudy)
+                AppPrefs.WEATHER_PRECIPITATION -> configContext.getText(R.string.weather_precipitation)
+                else -> configContext.getText(R.string.weather_clear)
+            }
+            tvUpdatedDateTitle.text = configContext.getText(R.string.updated_date_title)
+            btnUpdate.text = configContext.getText(R.string.button_update)
+            tvDropdownMenuTitle.text = configContext.getText(R.string.data_source)
+            toolbar.title = appPref.getLocationName(appPref.getCurrentLanguage())
+
+            if (vm.error.value != WeatherFragmentViewModel.NO_ERROR) {
+                tvError.text = when(vm.error.value) {
+                    WeatherFragmentViewModel.ERROR_CODE_LOCATION -> configContext.getText(R.string.location_error)
+                    WeatherFragmentViewModel.ERROR_CODE_OUTDATED_LOCATION -> configContext.getText(R.string.outdated_location_error)
+                    WeatherFragmentViewModel.ERROR_CODE_DATA_UPDATE -> configContext.getText(R.string.data_update_error)
+                    else -> ""
+                }
+            }
+        }
     }
 
     private fun getCurrentLocation(l: OnCurrentLocationListener) {
@@ -76,24 +201,45 @@ class WeatherFragment : Fragment(R.layout.fragment_weather) {
                 val provider = locManager.getBestProvider(criteria, true)
 
                 provider?.let { providerName ->
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                        locManager.getCurrentLocation(
-                            providerName,
-                            null,
-                            requireContext().mainExecutor
-                        ) {
-                            l.invoke(it)
-                        }
-                    } else {
-                        @Suppress("DEPRECATION")
-                        locManager.requestSingleUpdate(
-                            providerName,
-                            {
+
+                    locManager.getLastKnownLocation(providerName)?.let { lastLocation ->
+                        l.invoke(lastLocation)
+
+                    } ?: run {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                            locManager.getCurrentLocation(
+                                providerName,
+                                null,
+                                requireContext().mainExecutor
+                            ) {
                                 l.invoke(it)
-                            },
-                            Looper.getMainLooper()
-                        )
+                            }
+                        } else {
+
+                            val locationListener = object : LocationListener {
+                                override fun onLocationChanged(location: Location) {
+                                    l.invoke(location)
+                                }
+
+                                override fun onProviderDisabled(provider: String) {}
+                                override fun onProviderEnabled(provider: String) {}
+                            }
+
+                            @Suppress("DEPRECATION")
+                            locManager.requestSingleUpdate(
+                                providerName,
+                                locationListener,
+                                Looper.getMainLooper()
+                            )
+                        }
                     }
+                } ?: run {
+                    appPref.getLocationLatitude()?.let { lat ->
+                        vm.outdatedLocationError()
+                        val lon = appPref.getLocationLongitude()!!
+                        vm.updateData(latitude = lat, longitude = lon)
+
+                    } ?: vm.locationError()
                 }
             }
 
